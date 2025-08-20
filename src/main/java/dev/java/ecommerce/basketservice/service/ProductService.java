af885062751479e3403896fece575f883196a30a
NicolasCoiado <nicolasmcoiado@gmail.com>
2025-08-20 19:28:03 -0300

    feat: Implements redis for caching

--- src/main/java/dev/java/ecommerce/basketservice/service/ProductService.java
+++ src/main/java/dev/java/ecommerce/basketservice/service/ProductService.java
@@ -3,21 +3,28 @@ package dev.java.ecommerce.basketservice.service;
 import dev.java.ecommerce.basketservice.client.PlatziStoreClient;
 import dev.java.ecommerce.basketservice.client.response.PlatziProductResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProductService {
 
     private final PlatziStoreClient platziStoreClient;
 
+    @Cacheable(value = "products") //ANOTATE
     public List<PlatziProductResponse> getAllProducts(){
+        log.info("Getting all products");
         return platziStoreClient.getAllProducts();
     }
 
-    public PlatziProductResponse getProductsById(Long id){
-        return platziStoreClient.getProduct(id);
+    @Cacheable(value = "product", key = "#productId")
+    public PlatziProductResponse getProductsById(Long productId){
+        log.info("Getting product with id: {}", productId);
+        return platziStoreClient.getProduct(productId);
     }
 }
