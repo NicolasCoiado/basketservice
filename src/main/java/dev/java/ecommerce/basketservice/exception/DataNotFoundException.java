02e5eab7fe35ce248c9d508dea906f4c4fed02f9
NicolasCoiado <nicolasmcoiado@gmail.com>
2025-08-22 18:50:02 -0300

    feat: Add business and data not found exceptions with global handler

---
+++ src/main/java/dev/java/ecommerce/basketservice/exception/DataNotFoundException.java
@@ -0,0 +1,7 @@
+package dev.java.ecommerce.basketservice.exception;
+
+public class DataNotFoundException extends RuntimeException {
+    public DataNotFoundException(String message) {
+        super(message);
+    }
+}
